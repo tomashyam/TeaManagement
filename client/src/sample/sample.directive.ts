df96685f3f67acd0f4b28a2a6d61a47fe7aa1d96
tomashyam <tomashyam@gmail.com>
2017-09-27 18:04:11 +0300

    added sample component

---
+++ client/src/sample/sample.directive.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by hack on 27/09/2017.
+ */
+
+
+export class SampleComponent {
+    template = `<h1>aaa</h1>`;
+    // bindings: {
+    //     // Optionals hero: '='
+    // },
+    controller= sampleController;
+
+
+}
+
+export class sampleController {
+
+}
+
+
