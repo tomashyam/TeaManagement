d46b9f52b2c1a2f8fcd42e6ec4f33e6986de773f
Ela <eladror66@gmail.com>
2017-09-28 01:31:31 +0300

    servers table view

---
+++ client/src/servers/servers.directive.ts
@@ -0,0 +1,46 @@
+import {IAppDataService} from '../Utilities/appData.service'
+
+export class ServersComponent {
+    templateUrl = 'servers/servers.html';
+    controller = ServersController;
+    controllerAs = 'ctrl';
+}
+
+export class ServersController {
+    static $inject = ['$location'];
+
+    constructor(public $location: any) {
+
+    }
+
+    public gridOptions = {
+        data: [{
+            "statusDisplay": "Valid",
+            "hostname": "AS66",
+            "address": "128.0.0.1",
+            "project" : "SouthPark",
+            "environment" : "prod",
+            "username" : "Administrator",
+            "password" : "Aa123456",
+            "description" : "For my big load cache",
+            "OS" : "Linux",
+            "IsAliveUrl" : "http://localhost/isalive"
+        },
+            {
+                "statusDisplay": "Hold",
+                "hostname": "WS66",
+                "address": "128.0.0.6",
+                "project" : "SouthPark",
+                "environment" : "exer",
+                "username" : "Administrator",
+                "password" : "Aa123456",
+                "description" : "For my big load cache",
+                "OS" : "Windows",
+                "IsAliveUrl" : "http://localhost/isalive"
+            }],
+        urlSync: false
+    };
+}
+
+
+
