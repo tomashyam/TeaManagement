2ef94d9e3e2a0eb2c5574c9b2e4efd1e732faad9
Dor Elmaliach <dor.elmal11@gmail.com>
2017-09-27 14:46:36 +0300

    application sessions backend

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-var express = require('express');
+﻿var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var applicationSessions = require('./core/applicationSessions/applicationSessionsRoute');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/applicationSessions', applicationSessions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
